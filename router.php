1331c7311d10e7c7e8abba1ee7c68776075ba3c9
Zalo Hue <checkzalosavedata-hue@users.noreply.github.com>
2026-05-09 17:01:52 +0700

    Sửa lỗi router không tải được CSS và JS

--- router.php
+++ router.php
@@ -3,7 +3,19 @@
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
-    return false; // serve the requested resource as-is
+    $file = __DIR__ . '/public' . $uri;
+    $ext = pathinfo($file, PATHINFO_EXTENSION);
+    $mimeTypes = [
+        'css' => 'text/css',
+        'js'  => 'application/javascript',
+        'png' => 'image/png',
+        'jpg' => 'image/jpeg'
+    ];
+    if (isset($mimeTypes[$ext])) {
+        header('Content-Type: ' . $mimeTypes[$ext]);
+    }
+    readfile($file);
+    exit;
 } else if (preg_match('/^\/api\//', $uri)) {
     include __DIR__ . '/api.php';
 } else {
